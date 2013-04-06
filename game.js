440676b6e41cdb48e5fff36d86d8a9dacc3c3978
Ember Baker <erb325@gmail.com>
2013-04-06 11:27:29 -0400

    game commit
    the game js file commit

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ var marked;
 var content;
 var combinations;
 var turn=0;
-var canvas;
+var box;
 var squaresFilled;
 
 
@@ -30,7 +30,73 @@ window.onload=function(){
     }
 
 }
+function boxClicked(boxNumber){
+    box = "box"+boxNumber;
+    var b = document.getElementById(box);
+    var draw = b.getContext("2d");
 
+    if (marked[boxNumber-1] == false){
+        if (turn%2==0){
+            draw.beginPath();
+            draw.moveTo(10,10);
+            draw.lineTo(40,40);
+            draw.moveTo(40,10);
+            draw.lineTo(10,40);
+            draw.stroke();
+            draw.closePath();
+            content[boxNumber-1] = 'X';
+
+        }
+        else
+        {
+            draw.beginPath();
+            draw.arc(25,25,20,0, Math.PI*2, true);
+            draw.stroke();
+            draw.closePath();
+            content[boxNumber -1] = 'O';
+
+        }
+        turn++;
+
+        marked [boxNumber-1] = true;
+        squaresFilled++;
+        checkWinners (content[boxNumber-1]);
+
+        if (squaresFilled==9){
+            alert("The Game is Over!");
+            location.reload(true);
+        }
+        else
+        {
+            alert ("That space is alread occupied");
+
+        }
+    }
+}
+
+function checkWinners(symbol){
+    for (i = 0; i < combinations.length; i++){
+        if (content[combinations[i][0]]==symbol && content [combinations [i][1]]== symbol &&
+            content[combination[i][2]]==symbol){
+            alert (symbol + " Won!!");
+            playAgain();
+        }
+    }
+}
+
+function playAgain(){
+    var again = confirm ("Do you want to play again?");
+    if (again == true) {
+        alert("Ok ^^/>")  ;
+        location.reload(true);
+
+      }
+    else
+    {
+        alert(":(");
+
+    }
+}
 
 
 
