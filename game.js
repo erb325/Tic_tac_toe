6911eb1894ffaffcc08e8d7d3bdad0fbc24192aa
Ember Baker <erb325@gmail.com>
2013-04-06 10:55:27 -0400

    window load
    when the window loads things that need to happen first

--- game.js
+++ game.js
@@ -6,3 +6,31 @@
  * To change this template use File | Settings | File Templates.
  */
 
+var marked;
+var content;
+var combinations;
+var turn=0;
+var canvas;
+var squaresFilled;
+
+
+window.onload=function(){
+
+    marked = new Array();
+
+    content = new Array();
+
+    combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+    for(var i = 0; i <= 8; i++){
+
+        marked[i] = false;
+
+        content[i]='';
+    }
+
+}
+
+
+
+
